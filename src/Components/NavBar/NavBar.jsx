8b9e1ef5f2a29f30b875f0c135e5a6f995ca5d02
Suraj <surajthapliyal18@gmail.com>
2022-01-12 00:22:48 +0530

    stripe implemented and bugs done

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -68,7 +68,7 @@ const PrimarySearchAppBar = ({ totalItems }) => {
               height="25px"
               className={classes.image}
             />{" "}
-            Commerce.js
+            Shopclub
           </Typography>
           <div className={classes.grow} />
           {location.pathname === "/" && (
